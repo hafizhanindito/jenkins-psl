bbb70422b9562f3477b61eeb86336d772db62dc0
hafizhanindito <hafizpratama@gmail.com>
2021-01-23 12:20:30 +0700

    reset pipeline

--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -1,78 +1,18 @@
 #!/usr/bin/groovy
 package com.workshop
- 
+
 import com.workshop.Config
 import com.workshop.stages.*
- 
+
 def main(script) {
-   // Object initialization
-   c = new Config()
-   sprebuild = new prebuild()
-   sbuild = new build()
-   sdeploy = new deploy()
-   spostdeploy = new postdeploy()
- 
-   // Pipeline specific variable get from injected env
-   // Mandatory variable wil be check at details & validation steps
-   def repository_name = ("${script.env.repository_name}" != "null") ? "${script.env.repository_name}" : ""
-   def branch_name = ("${script.env.branch_name}" != "null") ? "${script.env.branch_name}" : ""
-   def git_user = ("${script.env.git_user}" != "null") ? "${script.env.git_user}" : ""
-   def docker_user = ("${script.env.docker_user}" != "null") ? "${script.env.docker_user}" : ""
-   def app_port = ("${script.env.app_port}" != "null") ? "${script.env.app_port}" : ""
-   def pr_num = ("${script.env.pr_num}" != "null") ? "${script.env.pr_num}" : ""
- 
-   // Timeout for Healtcheck
-   def timeout_hc = (script.env.timeout_hc != "null") ? script.env.timeout_hc : 10
- 
-   // Have default value
-   def docker_registry = ("${script.env.docker_registry}" != "null") ? "${script.env.docker_registry}" : "${c.default_docker_registry}"
- 
-   // Initialize docker tools
-   def dockerTool = tool name: 'docker', type: 'dockerTool'
- 
-   // Pipeline object
-   p = new Pipeline(
-       repository_name,
-       branch_name,
-       git_user,
-       docker_user,
-       app_port,
-       pr_num,
-       dockerTool,
-       docker_registry,
-       timeout_hc
-   )
- 
-   ansiColor('xterm') {
-       stage('Pre Build - Details') {
-           sprebuild.validation(p)
-           sprebuild.details(p)
-       }
- 
-       stage('Pre Build - Checkout & Test') {
-           sprebuild.checkoutBuildTest(p)
-       }
- 
-       stage('Build & Push Image') {
-           sbuild.build(p)
-       }
- 
-       stage('Merge') {
-           spostbuild.merge(p)
-       }
- 
-       stage('Deploy') {
-           sdeploy.deploy(p)
-       }
- 
-       stage('Service Healthcheck') {
-           spostdeploy.healthcheck(p)
-       }
- 
-       stage('Delete Old Image'){
-           spostdeploy.deleteOldImage(p)
-       }
-   }
+    // Object initialization
+
+    // Pipeline object
+
+    ansiColor('xterm') {
+        stage('Test stage') {
+            println "Hello!"
+        }
+    }
 }
- 
 return this
--- src/com/workshop/stages/postdeploy.groovy
+++ src/com/workshop/stages/postdeploy.groovy
@@ -1,27 +1,4 @@
 #!/usr/bin/groovy
 package com.workshop.stages
- 
+
 import com.workshop.Pipeline
- 
-def healthcheck(Pipeline p) {
-   def hostIp = sh script: "ip route show | awk '/default/ {print \$3}' | tr -d '\n'", returnStdout: true
- 
-   timeout(time: p.timeout_hc, unit: 'SECONDS'){
-       waitUntil(quiet: true) {
-           def response = sh script: "curl ${hostIp}:${p.app_port}/ping", returnStdout: true
-           if (response != "pong!"){
-               error("ERROR102 - Service is Unhealthy for last ${p.timeout_hc} Second")
-           } else {
-               println "Service is Healthy :D"
-               return true
-           }
-       }
-   }
-}
-def deleteOldImage(Pipeline p) {
-   withEnv(["PATH+DOCKER=${p.dockerTool}/bin"]){
-       def output = sh script: "docker rmi \$(docker images | grep -Ev 'latest|${BUILD_NUMBER}' | awk '/${p.docker_user}\\/${p.repository_name}/ {print \$1\":\"\$2}')", returnStdout: true
-       println "Remove old Image"
-       println output
-   }
-}
